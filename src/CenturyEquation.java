e6773d6cb2a2dfa82ec1bd43c839efc32cae18ab
Vladimir Tsvetkov <tsvvladimir95@me.com>
2014-12-22 11:12:45 +0300

    11

---
+++ src/CenturyEquation.java
@@ -0,0 +1,22 @@
+/**
+ * Created by vladimirtsvetkov on 22/12/14.
+ */
+public class CenturyEquation {
+    int k;
+    double ro, lambda;
+    double []u, d;
+
+
+    public CenturyEquation(int k, double ro, double lambda, double []u, double []d){
+        this.k = k;
+        this.ro = ro;
+        this.lambda = lambda;
+        this.u = u;
+        this.d = d;
+    }
+
+    public double [] count(double i ){
+        return null;
+    }
+    
+}
